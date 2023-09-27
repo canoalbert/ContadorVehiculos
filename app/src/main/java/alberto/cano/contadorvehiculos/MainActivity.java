9915a02161cccba338cc1d642a2474a23a0d4ec8
albertocanoroca <alberto.canoroca@cieep.com>
2023-09-27 12:20:49 +0200

    UPDATED

--- app/src/main/java/alberto/cano/contadorvehiculos/MainActivity.java
+++ app/src/main/java/alberto/cano/contadorvehiculos/MainActivity.java
@@ -3,19 +3,34 @@ package alberto.cano.contadorvehiculos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText textView1;
-    private EditText textView2;
-    private EditText textView3;
+    private TextView textView1;
+    private TextView textView2;
+    private TextView textView3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initializeComponents();
+    }
+
+    private void initializeComponents() {
+        textView1 = findViewById(R.id.textView1);
+        textView1 = findViewById(R.id.textView2);
+        textView3 = findViewById(R.id.textView3);
+
+    }
+    public void onClickBotones(View boton){
 
     }
 }
